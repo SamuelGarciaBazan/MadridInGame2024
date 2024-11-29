0fbf7604046638897fbaa0de79a3a479dfdc6619
SamuelGarciaBazan <samuga05@ucm.es>
2024-11-30 00:01:27 +0100

    relleno

---
+++ AlfombraJuego/Assets/Scripts/Turn/Nodo.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Nodo : MonoBehaviour
+{
+
+    //estados finales 
+    List<int> estadosGlobales = new List<int>();
+
+    //puede cambiar el tipo
+    List<int> modificadoresNegativos = new List<int>();
+
+    //figura que está colocada, cambiar por enum
+    int edificio = -1;
+
+
+  
+}
--- AlfombraJuego/Assets/Scripts/Turn/TurnManager.cs
+++ AlfombraJuego/Assets/Scripts/Turn/TurnManager.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class TurnManager : MonoBehaviour
 {
+
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
