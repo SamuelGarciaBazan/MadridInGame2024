80de657792be1071f81e8f86dbe8a54fd96bde28
LuisEnriqueCabelloBlanco <luisecab@ucm.es>
2024-11-30 00:29:54 +0100

    Baja mas duro que el pan de ayer

--- AlfombraJuego/Assets/Scripts/SelctionScript.cs
+++ AlfombraJuego/Assets/Scripts/SelctionScript.cs
@@ -11,6 +11,8 @@ public class SelctionScript : MonoBehaviour
     float heith=10;
     
     Transform objectReference;
+    Vector3 preDragPosition;
+
 
     public void drag(InputAction.CallbackContext context)
     {
@@ -18,14 +20,23 @@ public class SelctionScript : MonoBehaviour
         RaycastHit hit;
         if (context.performed&& Physics.Raycast(camray, out hit, 500, LayerMask.GetMask("Drageable")))
         {
+            preDragPosition = hit.transform.position;
             objectReference = hit.transform;
             reference.gameObject.SetActive(true);
             Debug.Log("TuViejaPick");
         }
         else if (context.canceled&&objectReference!=null)
         {
-            Physics.Raycast(camray, out hit, 500);
-            objectReference.position = hit.point+new Vector3(0, objectReference.GetComponent<Collider>().bounds.extents.y, 0);
+            if(Physics.Raycast(camray, out hit, 500, LayerMask.GetMask("Nodo")))
+            {
+                objectReference.position = hit.transform.position+
+                    new Vector3(0,hit.collider.bounds.extents.y,0)
+                    +new Vector3(0, objectReference.GetComponent<Collider>().bounds.extents.y, 0);
+            }
+            else
+            {
+                objectReference.position = preDragPosition;
+            }
             objectReference = null;
             Debug.Log("TuViejaDrop");
             reference.gameObject.SetActive(false);
@@ -45,7 +56,7 @@ public class SelctionScript : MonoBehaviour
     {
         Ray camray =Camera.main.ScreenPointToRay(Input.mousePosition); 
         RaycastHit hit;
-        if (Physics.Raycast(camray,out hit,500,LayerMask.GetMask("Floor")))
+        if (Physics.Raycast(camray, out hit, 500, LayerMask.GetMask("Floor","Drageable")))
         {
             reference.position = hit.point;
             if(objectReference != null)
