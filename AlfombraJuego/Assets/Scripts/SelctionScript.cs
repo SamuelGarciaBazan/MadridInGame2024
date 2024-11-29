258c67f0bc7a04b0644743afcfd88195ac920314
LuisEnriqueCabelloBlanco <luisecab@ucm.es>
2024-11-30 00:08:13 +0100

    drag&cock

--- AlfombraJuego/Assets/Scripts/SelctionScript.cs
+++ AlfombraJuego/Assets/Scripts/SelctionScript.cs
@@ -1,16 +1,43 @@
+using System;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class SelctionScript : MonoBehaviour
 {
-    Transform myTrasform;
     [SerializeField]
     Transform reference;
+    [SerializeField]
+    float heith=10;
+    
+    Transform objectReference;
+
+    public void drag(InputAction.CallbackContext context)
+    {
+        Ray camray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (context.performed&& Physics.Raycast(camray, out hit, 500, LayerMask.GetMask("Drageable")))
+        {
+            objectReference = hit.transform;
+            reference.gameObject.SetActive(true);
+            Debug.Log("TuViejaPick");
+        }
+        else if (context.canceled&&objectReference!=null)
+        {
+            Physics.Raycast(camray, out hit, 500);
+            objectReference.position = hit.point+new Vector3(0, objectReference.GetComponent<Collider>().bounds.extents.y, 0);
+            objectReference = null;
+            Debug.Log("TuViejaDrop");
+            reference.gameObject.SetActive(false);
+        }
+    }
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
+        objectReference = null;
+        reference.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,6 +48,10 @@ public class SelctionScript : MonoBehaviour
         if (Physics.Raycast(camray,out hit,500,LayerMask.GetMask("Floor")))
         {
             reference.position = hit.point;
+            if(objectReference != null)
+            {
+                objectReference.transform.position = new Vector3(0, heith, 0) + hit.point;
+            }
             Debug.Log("tuvieja "+hit.transform.name);
         }
     }
