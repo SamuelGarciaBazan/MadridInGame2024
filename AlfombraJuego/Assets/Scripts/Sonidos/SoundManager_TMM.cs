52ce7e47b89b9394b70089ca421a62f22b4ce321
rubyroja <147742438+rubyroja@users.noreply.github.com>
2024-12-01 05:41:29 +0100

    Sonido

---
+++ AlfombraJuego/Assets/Scripts/Sonidos/SoundManager_TMM.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager THIS;
+
+    AudioSource audioSource;
+
+    
+    public AudioClip[] sounds;
+
+    void Awake()
+    {
+        if (THIS == null)
+        {
+            THIS = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// Reproduce un hilo musical
+    /// </summary>
+    /// <param name="index"> Asigna el valor del indice asociado con el array Sounds </param>
+    public void PlaySoundByIndex(int index)
+    {
+        audioSource.PlayOneShot(sounds[index]);
+    }
+}
