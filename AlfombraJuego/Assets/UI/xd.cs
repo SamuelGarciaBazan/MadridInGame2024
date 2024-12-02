ad038db18e4bb6e5c03503bd04cd0d781d4e1b23
Luis <lparres@ucm.es>
2024-12-02 17:37:25 +0100

    botones para saltar a los niveles

--- AlfombraJuego/Assets/UI/xd.cs
+++ AlfombraJuego/Assets/UI/xd.cs
@@ -15,7 +15,7 @@ public class xd : MonoBehaviour
         
     }
 
-    public void xdd() {
-        SceneManager.LoadScene(2);
+    public void xdd(int n) {
+        SceneManager.LoadScene(n);
     }
 }
